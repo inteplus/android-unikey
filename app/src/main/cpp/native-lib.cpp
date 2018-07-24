e8c2e5c1e39a32f0b891b4ea2d1da90d48df3595
Minh-Tri Pham <pmtri80@gmail.com>
2018-07-24 22:19:57 +0100

    adding libunikey

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -7,6 +7,6 @@ JNICALL
 Java_com_foomum_minhtri_unikeyboard_MainActivity_stringFromJNI(
         JNIEnv *env,
         jobject /* this */) {
-    std::string hello = "Hello from C++";
+    std::string hello = "Hello from C++. I am Android-Unikey";
     return env->NewStringUTF(hello.c_str());
 }
